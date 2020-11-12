03913d72f67c74915dc9fbd083073b7764f95f88
Arpit <arpit.rai@viayam.today>
2020-11-12 14:18:54 +0530

    Changed the sizes according to Chrome 100% zoom

--- 3DAnimationProject/app.js
+++ 3DAnimationProject/app.js
@@ -3,23 +3,23 @@ const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 const title = document.querySelector('.title');
 const sneaker = document.querySelector('.sneaker img');
-const purchase = document.querySelector('.purchase button');
+const purchase = document.querySelector('.purchase');
 const description = document.querySelector('.info h3');
-const sizes = document.querySelector('.sizes')
+const sizes = document.querySelector('.sizes');
 
 //Animation Event
 container.addEventListener('mousemove', (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 //Animation In
-container.addEventListener('mousemove', (e) => {
+container.addEventListener('mouseenter', (e) => {
 card.style.transition =  "none";
-title.style.transform = "translateZ(100px)";
+title.style.transform = "translateZ(150px)";
 sneaker.style.transform = "translateZ(100px) rotate(-45deg)";
-purchase.style.transform = "translateZ(100px)";
+purchase.style.transform = "translateZ(130px)";
 description.style.transform = "translateZ(100px)";
 sizes.style.transform = "translateZ(100px)";
 });
@@ -33,4 +33,4 @@ container.addEventListener('mouseleave', (e) => {
     purchase.style.transform = "translateZ(0px)";
     sizes.style.transform = "translateZ(0px)";    
     description.style.transform = "translateZ(0px)";
-})
+});
